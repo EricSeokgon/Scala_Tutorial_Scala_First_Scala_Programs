e87760d13d48dc2f9e43be787d3b1ab799c68430
hadesyi <leesk55@gmail.com>
2018-03-17 01:35:44 +0900

    A mutable variable is declared with the keyword var and it must also be initialized immediately.

---
+++ src/Introduction/Mutablevariable.scala
@@ -0,0 +1,9 @@
+package Introduction
+
+object Mutablevariable {
+  def main(args: Array[String]): Unit = {
+    var stockPrice: Double = 100.0
+    stockPrice = 200.0
+    println(stockPrice);
+  }
+}
