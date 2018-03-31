4cc992b4632c9058efcd3bdc7f99b73e884a8669
hadesyi <leesk55@gmail.com>
2018-04-01 02:01:58 +0900

    Methods and variables also can be defined in code blocks that are denoted by curly braces:{ }.
    Code blocks may be nested.
    
    The result of a code block is the last line evaluated in the code block as shown in the following

---
+++ src/Introduction/CodeBlocks.scala
@@ -0,0 +1,18 @@
+package Introduction
+
+object CodeBlocks {
+
+  def meth1(): String = {
+    "hi"
+  }
+
+  def meth2(): String = {
+    val d = new java.util.Date()
+    d.toString()
+  }
+
+  def main(args: Array[String]) {
+    println(meth1)
+    println(meth2)
+  }
+}
