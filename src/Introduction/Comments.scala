e1e70ce57560893cb1466672a3715237a0a835a0
hadesyi <leesk55@gmail.com>
2018-04-12 02:18:36 +0900

    Scala Comments

---
+++ src/Introduction/Comments.scala
@@ -0,0 +1,17 @@
+package Introduction
+
+object Comments {
+  /*
+      This is a multiline comment:
+  */
+
+  // This is a single line comment
+
+  /*
+This is an outer comment
+/* And this comment
+is nested
+*/
+Outer comment
+*/
+}
