abf40166c878f33b3e4bec9ba2c5cf9fb7c6e567
hadesyi <leesk55@gmail.com>
2018-06-30 03:03:15 +0900

    Scala Numeric Types

---
+++ src/DataType/NumericTypes.scala
@@ -0,0 +1,12 @@
+package DataType
+
+object NumericTypes {
+  def main(args: Array[String]): Unit = {
+    val x: Byte = 30
+    println(x)
+    val y: Short = x
+    println(y)
+    val z: Double = y
+    println(z)
+  }
+}
--- src/DataType/Strings.scala
+++ src/DataType/Strings.scala
@@ -4,11 +4,5 @@ object Strings {
   def main(args: Array[String]): Unit = {
     val hello = "hell,o World!"
     println(hello)
-    val x: Byte = 30
-    println(x)
-    val y: Short = x
-    println(y)
-    val z: Double = y
-    println(z)
   }
 }
