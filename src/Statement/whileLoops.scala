8b17f8278a3f0872e8cb7aad5b043c8ddd5f7f80
hadesyi <leesk55@gmail.com>
2018-07-29 01:28:14 +0900

    Scala while Loops

---
+++ src/Statement/whileLoops.scala
@@ -0,0 +1,19 @@
+package Statement
+
+object whileLoops {
+  def main(args: Array[String]): Unit = {
+    import java.util.Calendar
+
+    def isFridayThirteen(cal: Calendar): Boolean = {
+      val dayOfWeek = cal.get(Calendar.DAY_OF_WEEK)
+      val dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
+      (dayOfWeek == Calendar.FRIDAY) && (dayOfMonth == 13)
+    }
+
+    while (!isFridayThirteen(Calendar.getInstance())) {
+      println("Today isn't Friday the 13th. Lame.")
+      Thread.sleep(86400000)
+    }
+  }
+
+}
