d424039f407a73cdd44e4e799e68a065c226c53b
hadesyi <leesk55@gmail.com>
2018-08-18 00:02:41 +0900

    Scala Match Expressions

--- src/Statement/ScalaMatchExpressions.scala
+++ src/Statement/ScalaMatchExpressions.scala
@@ -6,6 +6,12 @@ class ScalaMatchExpressions {
       case 44 => true// if we match 44,the result is true
       case _ => false// otherwise the result isfalse
     }
+
+    "CSS" match {
+      case "CSS"=> 45 // the result is 45 if we match "CSS"
+      case "Elwood" => 77
+      case _ => 0
+    }
   }
 
 }
