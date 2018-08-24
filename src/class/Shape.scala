aeb3f56c852fbbdfe2fdb743bdcb6bcb45632074
hadesyi <leesk55@gmail.com>
2018-08-25 00:30:48 +0900

    Scala Class

--- src/class/Shape.scala
+++ src/class/Shape.scala
@@ -4,3 +4,11 @@ class Shape {
   def area: Double = 0.0
 
 }
+
+class Rectangle(val width: Double, val height: Double) extends Shape {
+  override def area: Double = width * height
+}
+
+class Circle(val radius: Double) extends Shape {
+  override def area: Double = math.Pi * radius * radius
+}
