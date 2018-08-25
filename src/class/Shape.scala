2c6bc346bad7e8a027b24ade0f1cd3c01bf7e831
hadesyi <leesk55@gmail.com>
2018-08-26 01:07:46 +0900

    Scala Class

--- src/class/Shape.scala
+++ src/class/Shape.scala
@@ -1,14 +1,23 @@
 package `class`
 
-class Shape {
+abstract class Shape {
   def area: Double = 0.0
-
+  def draw(s:Shape)
 }
 
-class Rectangle(val width: Double, val height: Double) extends Shape {
+abstract class Rectangle(val width: Double, val height: Double) extends Shape {
   override def area: Double = width * height
+  val circle = draw(new Circle(3) {
+    override def draw(s: Shape): Unit = ???
+  })
+
+
 }
 
-class Circle(val radius: Double) extends Shape {
+abstract class Circle(val radius: Double) extends Shape {
   override def area: Double = math.Pi * radius * radius
+  val rectangle = draw(new Rectangle(2,3) {
+    override def draw(s: Shape): Unit = ???
+  })
 }
+
