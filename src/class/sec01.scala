0f1c09416700075655dfe54a239014f7e2839408
hadesyi <leesk55@gmail.com>
2018-09-23 02:05:54 +0900

    Scala Method Declaration

--- src/class/sec01.scala
+++ src/class/sec01.scala
@@ -1,6 +1,16 @@
 package `class`
 
 class sec01 {
-  def myMethod():String = "hadeslee.tistory.com"
+  def myMethod(): String = "hadeslee.tistory.com"
+
   def myOtherMethod() = "Moof"
+
+  def foo(a: Int): String = a.toString
+
+  def f2(a: Int, b: Boolean): String = if (b) a.toString else "false"
+
+  def list[T](p: T): List[T]
+  = p :: Nil
+  list(1)
+  list("Hello")
 }
