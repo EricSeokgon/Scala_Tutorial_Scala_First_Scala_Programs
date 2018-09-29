503804275adf86e61e3ac11c8546d824bfed476c
hadesyi <leesk55@gmail.com>
2018-09-30 00:33:14 +0900

    Extending Class

--- src/class/Vehicle.scala
+++ src/class/Vehicle.scala
@@ -1,8 +1,31 @@
 package `class`
 
-class Vehicle (speed : Int){
-  val mph :Int = speed
+class Vehicle(speed: Int) {
+  val mph: Int = speed
+
   def race() = println("Racing")
 
   new Vehicle(100)
+}
+
+class Car(speed: Int) extends Vehicle(speed) {
+  override val mph: Int = speed
+
+  override def race() = println("Racing Car")
+
+}
+
+class Bike(speed: Int) extends Vehicle(speed) {
+  override val mph: Int = speed
+
+  override def race() = println("Racing Bike")
+}
+
+object Main extends App {
+  val vehicle1 = new Car(200)
+  println(vehicle1.mph)
+  vehicle1.race()
+  val vehicle2 = new Bike(100)
+  println(vehicle2.mph)
+
 }
