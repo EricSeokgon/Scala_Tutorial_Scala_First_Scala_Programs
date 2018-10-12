16879066b0e659b2dce504bc4c4e368cc11627fa
hadesyi <leesk55@gmail.com>
2018-10-13 00:42:16 +0900

    Scala Tutorial - Scala Arrays

---
+++ src/Collection/sec01.scala
@@ -0,0 +1,12 @@
+package Collection
+
+object sec01 {
+  def main(args: Array[String]) {
+    var books = new Array[String](3)
+    books(0) = "Scala";
+    books(1) = "Java";
+    books(2) = "Groovy"
+    println(books(0))
+
+  }
+}
